37860c7f047a58635229e5a6600745670b950338
Chantal Justamond <cjustamm66@me.com>
2017-03-15 09:33:45 -0600

    difference done

---
+++ sum_of_squares.rb
@@ -0,0 +1,18 @@
+numbers = (1..100).to_a
+
+sum_of_squares = numbers.inject do |sum, item| 
+   sum + (item*item)
+end
+
+
+
+square_of_the_sum = numbers.reduce(:+) ** 2
+
+
+puts (square_of_the_sum - sum_of_squares)
+    
+  
+ 
+
+
+
