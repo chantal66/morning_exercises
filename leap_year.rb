c0df33b9093537f6200dbf1424a7095c39084452
Chantal Justamond <cjustamm66@me.com>
2017-03-21 09:25:26 -0600

    reverse a string is working

---
+++ leap_year.rb
@@ -0,0 +1,19 @@
+# require 'date'
+# class LeapYear
+#   attr_reader :year
+
+#   def initialize(year)
+#     @year = year
+#   end
+
+#   def leap_year?(year)
+#   (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)
+# end
+# end
+
+
+# puts @leap_year.leap_year?
+  
+p Date.new(2017,2,3) - 1
+   
+
---
+++ reverse_string.rb
@@ -0,0 +1,8 @@
+string = 'word'.split('')
+
+my_reverse = string.sort_by.with_index do |i, index|
+   -i
+end 
+
+p join_the_words = my_reverse.join
+
